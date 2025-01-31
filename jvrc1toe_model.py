b10b2493f8ac2f7a5ed46c8068e98721ad15abf8
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2025-01-31 23:15:46 +0900

    FIX: using Toe

--- jvrc1toe_model.py
+++ jvrc1toe_model.py
@@ -37,7 +37,7 @@ class JVRC1Toe(ru.ImportedRobotModel):
         toe.translate(fv(len_a, 0, 0))
         toe.rotate(-theta, coordinates.Y)
         toe.translate(fv(0.5*len_b, 0, 0))
-        self.registerEndEffector('llegt', ## end-effector
+        self.registerEndEffector('ltoe', ## end-effector
                                  'L_ANKLE_P', ## tip-link
                                  tip_link_to_eef = toe,
                                  joint_tuples = (('L_HIP_P', 'hip-p'),
@@ -48,7 +48,7 @@ class JVRC1Toe(ru.ImportedRobotModel):
                                                  ('L_ANKLE_P', 'ankle-p'),
                                                  )
                                  )
-        self.registerEndEffector('rlegt', ## end-effector
+        self.registerEndEffector('rtoe', ## end-effector
                                  'R_ANKLE_P', ## tip-link
                                  tip_link_to_eef = toe.copy(),
                                  joint_tuples = (('R_HIP_P', 'hip-p'),
@@ -59,7 +59,32 @@ class JVRC1Toe(ru.ImportedRobotModel):
                                                  ('R_ANKLE_P', 'ankle-p'),
                                                  )
                                  )
-        self.foot2toe = coordinates(fv(len_a + 0.5*len_b*math.cos(theta), 0, 0))
+        self.registerEndEffector('lmid', ## end-effector
+                                 'L_ANKLE_P', ## tip-link
+                                 tip_link_to_eef = coordinates(fv(len_a, 0, self.off_)),
+                                 joint_tuples = (('L_HIP_P', 'hip-p'),
+                                                 ('L_HIP_R', 'hip-r'),
+                                                 ('L_HIP_Y', 'hip-y'),
+                                                 ('L_KNEE', 'knee-p'),
+                                                 ('L_ANKLE_R', 'ankle-r'),
+                                                 ('L_ANKLE_P', 'ankle-p'),
+                                                 )
+                                 )
+        self.registerEndEffector('rmid', ## end-effector
+                                 'R_ANKLE_P', ## tip-link
+                                 tip_link_to_eef = coordinates(fv(len_a, 0, self.off_)),
+                                 joint_tuples = (('R_HIP_P', 'hip-p'),
+                                                 ('R_HIP_R', 'hip-r'),
+                                                 ('R_HIP_Y', 'hip-y'),
+                                                 ('R_KNEE', 'knee-p'),
+                                                 ('R_ANKLE_R', 'ankle-r'),
+                                                 ('R_ANKLE_P', 'ankle-p'),
+                                                 )
+                                 )
+        self.target_foot2toe = coordinates(fv(len_a + 0.5*len_b*math.cos(theta), 0, 0))
+        self.target_foot2mid = coordinates(fv(len_a, 0, 0))
+        self.foot2toe = coordinates().translate(fv(len_a, 0, 0)).rotate(-theta, coordinates.Y).translate(fv(0.5*len_b, 0, 0))
+        self.foot2mid = coordinates(fv(len_a, 0, 0))
         ## llegt.inverseKinematics( lleg.endEffector.transform(robot.foot2toe) ) ###
         self._makefoot(len_a, len_b, len_a, theta, width=width)##
         #### old points
@@ -94,6 +119,11 @@ class JVRC1Toe(ru.ImportedRobotModel):
         self.setFrame('ltoe_point1', 'L_ANKLE_P', ll)
         self.setFrame('ltoe_point2', 'L_ANKLE_P', lr.translate(fv(len_b, 0, 0)))
         self.setFrame('ltoe_point3', 'L_ANKLE_P', ll.translate(fv(len_b, 0, 0)))
+        ####
+        self._ltoe_=self.getLimb('ltoe')
+        self._rtoe_=self.getLimb('rtoe')
+        self._lmid_=self.getLimb('lmid')
+        self._rmid_=self.getLimb('rmid')
 
     def _makefoot(self, length_a, length_b, length_c, theta, width, ti=0.02):
         sth = math.sin(theta)
@@ -116,20 +146,24 @@ class JVRC1Toe(ru.ImportedRobotModel):
         jtl = self.robot.joint('L_ANKLE_P')
         jtl.clearShapeNodes()
         jtl.addShapeNode(trs)
-
     @property
-    def llegt(self):
-        return self.getLimb('llegt')
-
+    def ltoe(self):
+        return self._ltoe_
     @property
-    def rlegt(self):
-        return self.getLimb('rlegt')
+    def rtoe(self):
+        return self._rtoe_
+    @property
+    def lmid(self):
+        return self._lmid_
+    @property
+    def rmid(self):
+        return self._rmid_
 
 ### settings of model_file
-JVRC1.model_file = f'{os.path.dirname(__file__)}/JVRC-1/main.wrl'
+JVRC1Toe.model_file = f'{os.path.dirname(__file__)}/JVRC-1/main.wrl'
 
 ### robot_class: 
-robot_class = JVRC1
+robot_class = JVRC1Toe
 
 ### makeRobot(robot=None, **kwargs):
 def makeRobot(robot=None, item=True, world=True, **kwargs):
